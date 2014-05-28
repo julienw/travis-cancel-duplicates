177e6d47b5ab79b51ac325efcc2d7e24484e1460
Julien Wajsberg <felash@gmail.com>
2014-05-28 14:44:17 +0200

    work through pagination

--- travis-cancel-duplicates.rb
+++ travis-cancel-duplicates.rb
@@ -3,6 +3,16 @@
 
 require 'travis'
 
+def get_builds(repo, after_number = nil)
+  args = { :event_type => 'pull_request' }
+  args[:after_number] = after_number if after_number
+
+  builds = repo.builds(args)
+  pendingBuilds = builds.take_while { |b| !b.passed? }.to_a
+  pendingBuilds.concat(get_builds(repo, builds.last.number)) if builds.length > 0 && pendingBuilds.length == builds.length
+  return pendingBuilds
+end
+
 print('Travis token (run `travis token` to get it): ')
 Travis.access_token = gets.chomp
 puts "Logging in..."
@@ -11,9 +21,7 @@ puts "Hello #{Travis::User.current.name}!"
 puts "Finding repository mozilla-b2g/gaia"
 repo = Travis::Repository.find('mozilla-b2g/gaia')
 
-repo.builds(:event_type => 'pull_request')
-  .take_while { |b| !b.passed? }
-  .group_by { |b| b.pull_request_number }
+get_builds(repo).group_by { |b| b.pull_request_number }
   .each {
     |_, b|
     b.drop(1)
