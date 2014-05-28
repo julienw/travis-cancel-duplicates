7098730a2650fa476c6bf7342eeb41ac8f03f778
Julien Wajsberg <felash@gmail.com>
2014-05-28 13:54:22 +0200

    it works!

--- travis-cancel-duplicates.rb
+++ travis-cancel-duplicates.rb
@@ -3,6 +3,12 @@
 
 require 'travis'
 
+print('Travis token (run `travis token` to get it): ')
+Travis.access_token = gets.chomp
+puts "Logging in..."
+puts "Hello #{Travis::User.current.name}!"
+
+puts "Finding repository mozilla-b2g/gaia"
 repo = Travis::Repository.find('mozilla-b2g/gaia')
 
 repo.builds(:event_type => 'pull_request')
